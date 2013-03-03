ef2bd761824cba84ec95b54e27e12d29fe771548
Stan <stanseuruk@gmail.com>
2013-03-03 02:49:19 -0800

    Initial Commit

---
+++ easyPdf/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  easyPdf
+//
+//  Created by Stanislau on 3/3/13.
+//  Copyright (c) 2013 Stanislau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
