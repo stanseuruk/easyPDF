06f500758ed1ce09743f5215810908977c81a60d
Andrei <andrei@bittorrent.com>
2013-03-03 13:57:08 +0300

    Initial Commit

---
+++ EASYPDF/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EASYPDF
+//
+//  Created by Andrei Malyhin on 3/3/13.
+//  Copyright (c) 2013 Andrei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
